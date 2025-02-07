1d3d55ba94c842d1995c5e3b58fa3e66662f95c3
AlistairMaul <119766941+AlistairMaul@users.noreply.github.com>
2025-02-07 17:54:33 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -121,7 +121,7 @@ export default function Home() {
               className="flex cursor-pointer items-center justify-center whitespace-nowrap text-4xl shadow-2xl"
               gradientColor={"dark" === "dark" ? "#262626" : "#D9D9D955"}
             >
-              <a className="bg-gradient-to-r from-purple-900 to-blue-700 text-transparent bg-clip-text"  href="https://github.com/AlistairMaul/stocks">
+              <a className="bg-gradient-to-r from-purple-900 to-blue-700 text-transparent bg-clip-text"  href="https://github.com/AlistairMaul/ContagionSim">
                 Contagion Simulation
                 <img src="https://github-production-user-asset-6210df.s3.amazonaws.com/119766941/411024535-cd8a9b57-6ada-4308-b74b-aa2da3dec5d1.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20250207%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250207T224836Z&X-Amz-Expires=300&X-Amz-Signature=386287d6629236487d4f3625b5089c00fd838a68be0b88fb38e1e171861e4f6c&X-Amz-SignedHeaders=host" alt="" width="150" height="150"/>
               </a>
